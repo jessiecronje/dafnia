2e1c6b339880947035a3554297437425242683ff
Jessica Page <jepa7971@win.su.se>
2018-11-15 10:33:00 +0100

    version 1.01

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,27 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#'This function says hello to any person
+#'
+#'bla bla bla further description
+#'
+#'@param name A string of characters, preferably a name of a person who you want
+#'            to say hello to (default = world)
+#'@return Prints Hello and 'name' to the screen
+#'@example hello(Jess)
+#'@export
 
-hello <- function() {
-  print("Hello, world!")
+#hello world
+hello <- function(name = "world"){
+
+  print(paste("Hello", name))
+
 }
+
+
+#import data
+
+dafnia <- readRDS("dafnia.rds")
+
+#save data that's no longer compressed
+
+usethis::use_data(dafnia)
+
+devtools::document()
